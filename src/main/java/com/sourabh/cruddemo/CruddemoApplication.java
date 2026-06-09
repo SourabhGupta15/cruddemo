f3d7f9ef7b08350606ec7e94a22db59899e1adf5
SourabhGupta15 <guptasourabh500@gmail.com>
2026-06-10 02:22:51 +0530

    Added code to save multiple Student objects.

--- src/main/java/com/sourabh/cruddemo/CruddemoApplication.java
+++ src/main/java/com/sourabh/cruddemo/CruddemoApplication.java
@@ -18,8 +18,26 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		
+		// create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "john@cruddemo.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@cruddemo.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@cruddemo.com");
+
+		// save the student objects
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+		
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
